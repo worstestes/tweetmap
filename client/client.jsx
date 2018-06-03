c70b0f0f18d688edae8d270aa24bb1ae68385048
Blake Estes <worstestes@gmail.com>
2018-06-02 22:44:37 -0500

    available access to mlab database

--- client/client.jsx
+++ client/client.jsx
@@ -4,13 +4,18 @@ import axios from 'axios';
 
 class App extends Component {
 
+
+
   render() {
+
     return(
       <div className='App'>
         <h1>Hi, World!</h1>
       </div>
     );
   }
+
+  
 }
 
 export default App;
--- database/database.js
+++ database/database.js
@@ -1,24 +1,69 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
 let mongoPath = process.env.MONGODB_URI || 'mongodb://ohzone:0hzone@ds253879.mlab.com:53879/teamtwit';
-mongoose.connect(mongoPath, {
-  useMongoClient: true
-});
+mongoose.connect(mongoPath);
+let db = mongoose.connection;
+var Schema = mongoose.Schema;
+db.on('error', console.error.bind(console, 'connection error:')); 
+db.once('open', function() { console.log('Connection Established.') });
 
-var Twit = mongoose.model('Twit', mongoose.Schema({ state: String, text: String}, { collection : 'twitdata' }));   // collection name;
+let Data = mongoose.model('Twitdata', 
+               new Schema({ state: String, text: String}), 
+               'twitdata');
+               
+               
+               
+// console.log(Data.find().exec((err, res) => {
+//   console.log(res);
+//     }));
+
+// var map = function() {  
+//   var summary = this.message_soup_text;
+//   if (summary) { 
+//       // quick lowercase to normalize per your requirements
+//       summary = summary.toLowerCase().split(" "); 
+//       for (var i = summary.length - 1; i >= 0; i--) {
+//           // might want to remove punctuation, etc. here
+//           if (summary[i])  {      // make sure there's something
+//              emit(summary[i], 1); // store a 1 for each word
+//           }
+//       }
+//   }
+// };
+
+// var reduce = function( key, values ) {    
+//   var count = 0;    
+//   values.forEach(function(v) {            
+//       count +=v;    
+//   });
+//   return count;
+// }
+
+// full thing
+Data.mapReduce(map, reduce, { out: "word_count" })
+
+
+// > db.word_count.find().sort({value:-1})
+
+
+
+
+
 
 const save = async () => {
   var variable = new schemaName({
     id: '',
     name: '',
   });
-  variable.save()
+  variable.save();
 };
 
-const find = (callback) => {
-  collectionName
-    .find({})
-    .sort('')
-    .exec((err, data) => {
-      callback(data);
-    });
+// const find = (name, callback) => {
+//     name
+//     .find({})
+//     .sort('')
+//     .exec((err, data) => {
+//       callback(data);
+//     });
+
+// }
